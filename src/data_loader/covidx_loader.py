7e37416a48d6b6b0d4afbb3d098dc2afc06f41bd
guipaiva <guipaivagpp@gmail.com>
2021-04-26 11:40:43 -0300

    Convert indentation to tabs

--- src/data_loader/covidx_loader.py
+++ src/data_loader/covidx_loader.py
@@ -3,38 +3,38 @@ from base.base_loader import BaseLoader
 import pandas as pd
 
 class CovidxLoader(BaseLoader):
-    def __init__(self, directory, label_dir, im_shape, batch_size=32):
-        super(CovidxLoader, self).__init__(directory, im_shape, batch_size, label_dir)
-        self.generator = ImageDataGenerator(rescale=1./255, validation_split=0.2)
-        self.dataframe = pd.read_csv(label_dir)
+	def __init__(self, directory, label_dir, im_shape, batch_size=32):
+		super(CovidxLoader, self).__init__(directory, im_shape, batch_size, label_dir)
+		self.generator = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+		self.dataframe = pd.read_csv(label_dir)
 
-    def get_train_ds(self):
-        train_ds = self.generator.flow_from_dataframe(
-            dataframe=self.dataframe,
-            directory=self.directory,
-            x_col='image',
-            y_col='label',
+	def get_train_ds(self):
+		train_ds = self.generator.flow_from_dataframe(
+			dataframe=self.dataframe,
+			directory=self.directory,
+			x_col='image',
+			y_col='label',
 			class_mode='binary',
-            batch_size=self.batch_size,
-            seed=36,
-            subset='training',
+			batch_size=self.batch_size,
+			seed=36,
+			subset='training',
 			target_size=self.im_shape[:-1]
-        )
+		)
 
 		return train_ds
 
-    def get_validation_ds(self):
-        train_ds = self.generator.flow_from_dataframe(
-            dataframe=self.dataframe,
-            directory=self.directory,
-            x_col='image',
-            y_col='label',
+	def get_validation_ds(self):
+		train_ds = self.generator.flow_from_dataframe(
+			dataframe=self.dataframe,
+			directory=self.directory,
+			x_col='image',
+			y_col='label',
 			class_mode='binary',
-            batch_size=self.batch_size,
-            seed=36,
-            subset='validation',
+			batch_size=self.batch_size,
+			seed=36,
+			subset='validation',
 			target_size=self.im_shape[:-1]
-        )
+		)
 
 		return validation_ds
 	
--- src/data_loader/sirm_loader.py
+++ src/data_loader/sirm_loader.py
@@ -4,28 +4,28 @@ from base.base_loader import BaseLoader
 
 class SirmLoader(BaseLoader):
 
-    def __init__(self, directory, im_shape, batch_size=32):
-        super(SirmLoader, self).__init__(directory, im_shape, batch_size)
-        self.generator = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+	def __init__(self, directory, im_shape, batch_size=32):
+		super(SirmLoader, self).__init__(directory, im_shape, batch_size)
+		self.generator = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
-    def get_train_ds(self):
-        train_ds = self.generator.flow_from_directory(
-            directory=self.directory,
-            class_mode='categorical',
-            batch_size=self.batch_size,
-            target_size=self.im_shape[:-1],
-            subset='training'
-        )
+	def get_train_ds(self):
+		train_ds = self.generator.flow_from_directory(
+			directory=self.directory,
+			class_mode='categorical',
+			batch_size=self.batch_size,
+			target_size=self.im_shape[:-1],
+			subset='training'
+		)
 
-        return train_ds
+		return train_ds
 
-    def get_validation_ds(self):
-        vald_ds = self.generator.flow_from_directory(
-            directory=self.directory,
-            class_mode='categorical',
-            batch_size=self.batch_size,
-            target_size=self.im_shape[:-1],
-            subset='validation'
-        )
+	def get_validation_ds(self):
+		vald_ds = self.generator.flow_from_directory(
+			directory=self.directory,
+			class_mode='categorical',
+			batch_size=self.batch_size,
+			target_size=self.im_shape[:-1],
+			subset='validation'
+		)
 
-        return vald_ds
+		return vald_ds
