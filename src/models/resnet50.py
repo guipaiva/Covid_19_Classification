94d9d1451f7936a5a36c97ddf6ec5e16ea76567d
guipaiva <guipaivagpp@gmail.com>
2021-05-13 19:11:41 -0300

    (Re)Built resnet50 according to TODO issues

--- src/models/resnet50.py
+++ src/models/resnet50.py
@@ -1,50 +1,53 @@
 import tensorflow as tf
 import tensorflow.keras.applications as applications
+from tensorflow.keras.applications.resnet import preprocess_input
 from base.base_model import BaseModel
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Lambda
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-class ResNet50V2(BaseModel):
-    def __init__(self, im_shape, transfer_learn, classes):
-        name = 'ResNet50'
-        super(ResNet50V2, self).__init__(name, im_shape, classes, transfer_learn)
-        
-        self.build_model()
-        
-    def build_model(self):
-        if self.transfer_learn:
-            self.model = tf.keras.Sequential()
-            resnet = applications.ResNet50V2(
-                include_top=False,
-                input_shape=self.im_shape
-            )
-            for layer in resnet.layers:
-                layer.trainable = False
-            self.model.add(resnet)
+class ResNet50(BaseModel):
+	def __init__(self, im_shape, transfer_learn, classes):
+		name = 'ResNet50'
+		super(ResNet50, self).__init__(name, im_shape, classes, transfer_learn)
+		self.activation_function = 'sigmoid' if classes == 2 else 'softmax'
+		self.weights = 'imagenet' if self.transfer_learn else None
+		self.layers_trainable = False if self.transfer_learn else True
+		self.loss = 'binary_crossentropy' if classes == 2 else 'categorical_crossentropy'
+		self.build_model()
 
-            # FC Layers
-            self.model.add(Flatten())
-            self.model.add(Dense(512, activation='relu'))
-            self.model.add(Dropout(.5))
+	def build_model(self):
 
-            # Classes
-            if(self.classes == 2)
-                self.model.add(Dense(self.classes, activation='sigmoid'))
-            else:
-                self.model.add(Dense(self.classes, activation = 'softmax'))
-        else:
-            self.model = applications.ResNet50V2(
-				include_top = True,
-				weights = None,
-				classes = self.classes
-            )
+		self.model = tf.keras.Sequential()
 
-        self.model.compile(
-            optimizer='adam',
-            loss='binary_crossentropy',
-            metrics=['accuracy']
-        )
+		self.model.add(Lambda(preprocess_input, input_shape = (224,224,3)))
 
-        print('Model built')
+		resnet = applications.ResNet50(
+			include_top=False,
+			input_shape=self.im_shape,
+			weights= self.weights
+		)
+
+		for layer in resnet.layers:
+			layer.trainable = self.layers_trainable
+
+		self.model.add(resnet)
+
+		# FC Layers
+		self.model.add(GlobalAveragePooling2D())
+		self.model.add(
+			Dense(
+				units=self.classes,
+				activation=self.activation_function,
+				name='predictions'
+			),
+		)
+
+		self.model.compile(
+			optimizer='adam',
+			loss=self.loss,
+			metrics=['accuracy']
+		)
+
+		print('Model built')
