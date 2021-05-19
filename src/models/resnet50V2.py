992bc656e13ca75c0eaf4e8e65a1789255f918a0
guipaiva <guipaivagpp@gmail.com>
2021-05-19 20:51:54 -0300

    Rebuilt resnetv2 and vgg16

--- src/models/resnet50V2.py
+++ src/models/resnet50V2.py
@@ -1,50 +1,55 @@
 import tensorflow as tf
 import tensorflow.keras.applications as applications
+from tensorflow.keras.applications.resnet_v2 import preprocess_input
 from base.base_model import BaseModel
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.layers import Dense, Lambda, GlobalAveragePooling2D
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 class ResNet50V2(BaseModel):
-    def __init__(self, im_shape, transfer_learn, classes):
-        name = 'ResNet50'
-        super(ResNet50V2, self).__init__(name, im_shape, classes, transfer_learn)
-        
-        self.build_model()
-        
-    def build_model(self):
-        if self.transfer_learn:
-            self.model = tf.keras.Sequential()
-            resnet = applications.ResNet50V2(
-                include_top=False,
-                input_shape=self.im_shape
-            )
-            for layer in resnet.layers:
-                layer.trainable = False
-            self.model.add(resnet)
+	def __init__(self, im_shape, transfer_learn, classes):
+		name = 'ResNet50V2'
+		super(ResNet50V2, self).__init__(name, im_shape, transfer_learn)
+		self.classes = classes
+		self.activation_function = 'sigmoid' if classes == 2 else 'softmax'
+		self.weights = 'imagenet' if self.transfer_learn else None
+		self.layers_trainable = False if self.transfer_learn else True
+		self.loss = 'binary_crossentropy' if classes == 2 else 'categorical_crossentropy'
 
-            # FC Layers
-            self.model.add(Flatten())
-            self.model.add(Dense(512, activation='relu'))
-            self.model.add(Dropout(.5))
+		self.check_shape()
+		self.build_model()
 
-            # Classes
-            if(self.classes == 2)
-                self.model.add(Dense(self.classes, activation='sigmoid'))
-            else:
-                self.model.add(Dense(self.classes, activation = 'softmax'))
-        else:
-            self.model = applications.ResNet50V2(
-				include_top = True,
-				weights = None,
-				classes = self.classes
-            )
+	def build_model(self):
+		print('Building {}...'.format(self.name))
+		self.model = tf.keras.Sequential()
+		self.model.add(Lambda(preprocess_input, input_shape=self.im_shape, name = 'preproc'))
 
-        self.model.compile(
-            optimizer='adam',
-            loss='binary_crossentropy',
-            metrics=['accuracy']
-        )
+		resnet = applications.ResNet50V2(
+			include_top=False,
+			input_shape=self.im_shape,
+			weights=self.weights
+		)
 
-        print('Model built')
+		for layer in resnet.layers:
+			layer.trainable = self.layers_trainable
+
+		self.model.add(resnet)
+
+		# FC Layers
+		self.model.add(GlobalAveragePooling2D(name='avg_pool'))
+		self.model.add(
+			Dense(
+				units=self.classes,
+				activation=self.activation_function,
+				name='predictions'
+			),
+		)
+
+		self.model.compile(
+			optimizer='adam',
+			loss=self.loss,
+			metrics=['accuracy']
+		)
+
+		print('Model built')
--- src/models/vgg16.py
+++ src/models/vgg16.py
@@ -1,45 +1,70 @@
 import tensorflow as tf
 import tensorflow.keras.applications as applications
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.layers import Flatten, Dense, Lambda, Flatten
 from base.base_model import BaseModel
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+
 class VGG16(BaseModel):
-    def __init__(self, im_shape, classes, transfer_learn):
+    def __init__(self, im_shape, transfer_learn, classes):
         name = 'VGG16'
-        super(VGG16, self).__init__(name, im_shape, classes, transfer_learn)
+        super(VGG16, self).__init__(name, im_shape, transfer_learn)
+        self.classes = classes
+        self.activation_function = 'sigmoid' if classes == 2 else 'softmax'
+        self.weights = 'imagenet' if self.transfer_learn else None
+        self.layers_trainable = False if self.transfer_learn else True
+        self.loss = 'binary_crossentropy' if classes == 2 else 'categorical_crossentropy'
+
+        self.check_shape()
         self.build_model()
 
     def build_model(self):
-		if self.transfer_learn:
-			self.model = tf.keras.Sequential()
-			vgg = applications.VGG16(
-                include_top=False, 
-                input_shape=self.im_shape
+        print('Building {}...'.format(self.name))
+        self.model = tf.keras.Sequential()
+        self.model.add(
+            Lambda(preprocess_input, input_shape=self.im_shape, name='preproc'))
+
+        vgg = applications.VGG16(
+            include_top=False,
+            input_shape=self.im_shape,
+            weights=self.weights
+        )
+
+        for layer in vgg.layers:
+            layer.trainable = self.layers_trainable
+
+        self.model.add(vgg)
+
+        # FC layers
+        self.model.add(Flatten(name='Flatten'))
+        self.model.add(
+            Dense(
+                units=4096,
+                activation='relu',
+                name='FC1'
             )
-			for layer in vgg.layers:
-				layer.trainable = False
-
-			self.model.add(vgg)
-
-			self.model.add(Flatten())
-			self.model.add(Dense(1024, activation='relu'))
-			self.model.add(Dropout(.5))
-            #Prediction
-            if classes == 2:
-			    self.model.add(Dense(self.classes, activation='sigmoid'))
-            else:
-                self.model.add(Dense(self.classes, activation='softmax'))
-		else:
-			self.model = applications.VGG16(
-				include_top = True,
-				weights = None,
-				classes = self.classes
-			)
+        )
+        self.model.add(
+            Dense(
+                units=4096,
+                activation='relu',
+                name='FC2'
+            )
+        )
+        self.model.add(
+            Dense(
+                units=self.classes,
+                activation=self.activation_function,
+                name='predictions'
+            )
+        )
 
         self.model.compile(
             optimizer='adam',
-            loss='binary_crossentropy',
+            loss=self.loss,
             metrics=['accuracy']
         )
+
+        print('Model built')
