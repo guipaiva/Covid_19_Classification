20627288120fd82df56a023746a2a1d48e797f46
guipaiva <guipaivagpp@gmail.com>
2021-05-19 21:09:35 -0300

    Rebuilt densenet

--- src/models/densenet121.py
+++ src/models/densenet121.py
@@ -1,53 +1,55 @@
 import tensorflow as tf
 import tensorflow.keras.applications as applications
+from tensorflow.keras.applications.densenet import preprocess_input
 from base.base_model import BaseModel
-from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Lambda
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 class DenseNet121(BaseModel):
-	def __init__(self, im_shape, classes, transfer_learn):
-		name = 'DenseNet121'
-		super(DenseNet121, self).__init__(name, im_shape, classes, transfer_learn)
+    def __init__(self, im_shape, classes, transfer_learn):
+        name = 'DenseNet121'
+        super(DenseNet121, self).__init__(name, im_shape, transfer_learn)
+        self.classes = classes
+        self.activation_function = 'sigmoid' if classes == 2 else 'softmax'
+        self.weights = 'imagenet' if self.transfer_learn else None
+        self.layers_trainable = False if self.transfer_learn else True
+        self.loss = 'binary_crossentropy' if classes == 2 else 'categorical_crossentropy'
 
-		self.build_model()
+        self.check_shape()
+        self.build_model()
 
-	def build_model(self):
-		if self.transfer_learn:
-			self.model = tf.keras.Sequential()
-			densenet = applications.DenseNet121(
-				include_top=False, 
-				input_shape=self.im_shape
-			)
+    def build_model(self):
+        print('Building {}...'.format(self.name))
+        self.model = tf.keras.Sequential()
+        self.model.add(
+            Lambda(preprocess_input, input_shape=self.im_shape, name='preproc'))
 
-			for layer in densenet.layers:
-				layer.trainable = False
-			self.model.add(densenet)
+        densenet = applications.DenseNet121(
+            include_top=False,
+            input_shape=self.im_shape,
+            weights=self.weights
+        )
 
-			# FC Layers
-			self.model.add(Flatten())
-			self.model.add(Dense(512, activation='relu'))
-			self.model.add(Dropout(.5))
+        for layer in densenet.layers:
+            layer.trainable = self.layers_trainable
+        self.model.add(densenet)
 
-			# Classes
-			if self.classes == 2:
-				self.model.add(Dense(self.classes, activation = 'sigmoid'))
-			else:
-				self.model.add(Dense(self.classes, activation='softmax'))
+        # FC Layers
+        self.model.add(GlobalAveragePooling2D(name='avg_pool'))
+        self.model.add(
+            Dense(
+                units=self.classes,
+                activation=self.activation_function,
+                name='predictions'
+            ),
+        )
 
-		else:
-			self.model = applications.DenseNet121(
-				include_top = True,
-				weights = None,
-				classes = self.classes
-			)
+        self.model.compile(
+            optimizer='adam',
+            loss=self.loss,
+            metrics=['accuracy']
+        )
 
-		self.model.compile(
-			optimizer='adam',
-			loss='binary_crossentropy',
-			metrics=['accuracy']
-		)
-		print('Model built')
-
-		
+        print('Model built')
