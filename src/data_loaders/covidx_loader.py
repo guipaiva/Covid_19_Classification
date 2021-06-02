d0bc56c3a3f7e9d9d7d63aee92ebeeb116880bd2
guipaiva <guipaivagpp@gmail.com>
2021-06-01 22:59:42 -0300

    implemented multiclass option on loader

--- src/data_loaders/covidx_loader.py
+++ src/data_loaders/covidx_loader.py
@@ -3,10 +3,11 @@ from base.base_loader import BaseLoader
 import pandas as pd
 
 class CovidxLoader(BaseLoader):
-	def __init__(self, directory, im_shape, label_dir, batch_size=32):
-		super(CovidxLoader, self).__init__(directory, im_shape, batch_size, label_dir)
+	def __init__(self, directory, im_shape, label_dir, class_mode, batch_size=32):
+		super(CovidxLoader, self).__init__(directory, im_shape, batch_size)
 		self.generator = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 		self.dataframe = pd.read_csv(label_dir)
+		self.class_mode = class_mode
 
 	def get_train_ds(self):
 		train_ds = self.generator.flow_from_dataframe(
@@ -14,8 +15,8 @@ class CovidxLoader(BaseLoader):
 			directory=self.directory,
 			x_col='image',
 			y_col='label',
-			class_mode='binary',
-			batch_size=self.batch_size,
+			class_mode = self.class_mode,
+			batch_size = self.batch_size,
 			seed=36,
 			subset='training',
 			target_size=self.im_shape[:-1]
@@ -24,17 +25,16 @@ class CovidxLoader(BaseLoader):
 		return train_ds
 
 	def get_validation_ds(self):
-		train_ds = self.generator.flow_from_dataframe(
+		validation_ds = self.generator.flow_from_dataframe(
 			dataframe=self.dataframe,
 			directory=self.directory,
 			x_col='image',
 			y_col='label',
-			class_mode='binary',
-			batch_size=self.batch_size,
+			class_mode = self.class_mode,
+			batch_size = self.batch_size,
 			seed=36,
 			subset='validation',
 			target_size=self.im_shape[:-1]
 		)
 
 		return validation_ds
-	
