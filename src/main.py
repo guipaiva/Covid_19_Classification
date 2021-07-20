a5debc6c0c66f224907ee60efa71443bec34bd81
guipaiva <guipaivagpp@gmail.com>
2021-07-20 18:25:19 -0300

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from trainers.simple_trainer import SimpleTrainer
 from models import vgg16, xception, densenet121, resnet50V2, resnet50
 from utils.definitions import DATA_DIR
 from data_loaders import covidx_loader
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 
 if __name__ == "__main__":
-	dataset_dir = os.path.join(DATA_DIR, 'COVIDx')
+	dataset_dir = os.path.join(DATA_DIR, 'COVIDx_binary')
 	train_dir = os.path.join(dataset_dir, 'train')
 	label_dir = os.path.join(dataset_dir, 'train.txt')
 	im_shape = (224,224,3)
@@ -16,7 +17,7 @@ if __name__ == "__main__":
 	validation_data = loader.get_validation_ds()
 	data = {'train': train_data, 'validation': validation_data}
 
-	model = vgg16.VGG16(im_shape, True, 1)
+	model = resnet50V2.ResNet50V2(im_shape, True, 1)
 	
 	trainer = SimpleTrainer(model.model, model.name, data, epochs = 12)
 
