7aa6f2df416a4225eab3f60ad89db00da5fce04f
guipaiva <guipaivagpp@gmail.com>
2021-08-10 17:58:11 -0300

    Splited binary and multiclass training

---
+++ src/trainers/binary_trainer.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+
+import numpy as np
+import tensorflow as tf
+from base.base_trainer import BaseTrainer
+from sklearn.utils.class_weight import compute_class_weight
+from utils.definitions import ROOT_DIR
+
+
+class BinaryTrainer(BaseTrainer):
+    def __init__(self, model, name, data, epochs):
+        super(BinaryTrainer, self).__init__(model, name, data, epochs)
+        self.log_dir = os.path.join(ROOT_DIR, 'logs')
+        self.model_name = name
+
+        self.class_weight = dict(enumerate(compute_class_weight(class_weight='balanced', classes=np.unique(
+            data['train'].classes), y=data['train'].classes)))
+
+    def train(self):
+        tb_folder = "binary/" + self.name + "/" + datetime.datetime.now().strftime("%d%m")
+        tb_dir = os.path.join(self.log_dir, tb_folder)
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(
+            log_dir=tb_dir, histogram_freq=1)
+		#TODO: Add confusion matrix, F1-Score, ROC AUC
+
+        history = self.model.fit(
+            self.data["train"],
+            validation_data=self.data["validation"],
+            callbacks=[tensorboard_callback],
+            class_weight=self.class_weight,
+            epochs=self.epochs,
+            verbose=1
+        )
+
+        return history
