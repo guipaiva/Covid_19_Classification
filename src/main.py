580dcb33c5c716eda130d2dc5f1a68fdaa7fd609
guipaiva <guipaivagpp@gmail.com>
2021-08-11 23:10:28 -0300

    adjusted for multclass classification

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
+from keras import backend as K
+from utils.definitions import DATA_DIR, LOGS_DIR
+from trainers import binary_trainer, multiclass_trainer
+from models import densenet121, resnet50, resnet50V2, vgg16, xception
+from data_loaders import covidx_loader
+import utils.tools as tools
+import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-import numpy as np
-import utils.tools as tools
-from data_loaders import covidx_loader
-from models import densenet121, resnet50, resnet50V2, vgg16, xception
-from trainers import binary_trainer, multiclass_trainer
-from utils.definitions import DATA_DIR, LOGS_DIR
-from keras import backend as K
 
 
 if __name__ == "__main__":
@@ -18,21 +17,25 @@ if __name__ == "__main__":
     train_dir = os.path.join(dataset_dir, 'train')
     label_dir = os.path.join(dataset_dir, 'train.txt')
     im_shape = (224, 224, 3)
-    loader = covidx_loader.CovidxLoader(
-        train_dir, im_shape, label_dir, 'categorical')
+
+    loader = covidx_loader.CovidxLoader(directory=train_dir,
+                                        im_shape=im_shape,
+                                        label_dir=label_dir,
+                                        class_mode='categorical')
 
     train_data = loader.get_train_ds()
     print(train_data.class_indices)
     validation_data = loader.get_validation_ds()
     data = {'train': train_data, 'validation': validation_data}
 
-    #TODO: make train.py
+    # TODO: make train.py
 
     for model in models:
-        model = model(im_shape, True, 1)
-        trainer = multiclass_trainer.MulticlassTrainer(model.model, model.name, data, epochs=12)
+        model = model(im_shape=im_shape, transfer_learn=True, classes=3)
+        trainer = multiclass_trainer.MulticlassTrainer(
+            model.model, model.name, data, epochs=12)
         history = trainer.train()
-        logs_dir = os.path.join(LOGS_DIR, *['raw','multiclass'])
+        logs_dir = os.path.join(LOGS_DIR, *['raw', 'multiclass'])
         tools.write_raw(history.history, LOGS_DIR, model.name)
         print(model.name + ' Trained\n\n')
         K.clear_session()
